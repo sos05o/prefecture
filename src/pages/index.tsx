119e718c53c2195e1caa20fefe01ff3f71c175d8
sos05o <r.arihara.sys20@morijyobi.ac.jp>
2022-12-22 17:28:28 +0900

    [Style] Prettierによる整形
    Prettierを使用した整形を行いました。

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
-import React, {useEffect, useState} from 'react'
-import Graph, {GraphProps} from "@/src/components/graph/Graph"
+import React, { useEffect, useState } from 'react'
+import Graph, { GraphProps } from '@/src/components/graph/Graph'
 import PrefectureCheckBoxes from '@/src/components/prefectureCheckBox/CheckBoxes'
-import {GetPopulationFunction, GetPrefectureFunctions} from '@/src/functions/getDatas'
-import {Prefecture} from '@/src/types/type'
+import { GetPopulationFunction, GetPrefectureFunctions } from '@/src/functions/getDatas'
+import { Prefecture } from '@/src/types/type'
 
 export interface checkListProps extends Prefecture {
   checked: boolean
@@ -10,19 +10,19 @@ export interface checkListProps extends Prefecture {
 
 const Index = () => {
   const [checkList, setCheckList] = useState<checkListProps[]>([])
-  const [dataList, setDataList] = useState<GraphProps["data"]>([])
+  const [dataList, setDataList] = useState<GraphProps['data']>([])
 
-  const {prefecture, isLoading} = GetPrefectureFunctions()
+  const { prefecture, isLoading } = GetPrefectureFunctions()
 
   const toggleCheckFlgHandler = (prefCode: Prefecture['prefCode']) => {
     const fixList = checkList.map((pref) =>
-      pref.prefCode === prefCode ? {...pref, checked: !pref.checked} : pref,
+      pref.prefCode === prefCode ? { ...pref, checked: !pref.checked } : pref,
     )
     setCheckList(fixList)
   }
 
   const getPopulationHandler = (prefCode: number) => {
-    const {population} = GetPopulationFunction(prefCode)
+    const { population } = GetPopulationFunction(prefCode)
     return population
   }
 
@@ -40,32 +40,37 @@ const Index = () => {
   }, [isLoading, prefecture])
 
   useEffect(() => {
-    const ary = checkList.filter(props => props.checked).map(props => {
-      return {
-        prefName: props.prefName,
-        prefCode: props.prefCode
-      }
-    })
-    const dataPrefNameList = dataList.map(props => props.prefName)
+    const ary = checkList
+      .filter((props) => props.checked)
+      .map((props) => {
+        return {
+          prefName: props.prefName,
+          prefCode: props.prefCode,
+        }
+      })
+    const dataPrefNameList = dataList.map((props) => props.prefName)
 
-    const getPrefCode = ary.filter(props => dataPrefNameList.indexOf(props.prefName) === -1)[0]
+    const getPrefCode = ary.filter((props) => dataPrefNameList.indexOf(props.prefName) === -1)[0]
 
     if (getPrefCode !== undefined) {
-      getPopulationHandler(getPrefCode.prefCode).then(res => {
+      getPopulationHandler(getPrefCode.prefCode).then((res) => {
         if (res !== undefined) {
-          setDataList([...dataList, {prefName: getPrefCode.prefName, population: res.result.data}])
+          setDataList([
+            ...dataList,
+            { prefName: getPrefCode.prefName, population: res.result.data },
+          ])
         }
       })
     } else {
-      const aryPrefNameList = ary.map(props => props.prefName)
-      const remainAry = dataList.filter(props => aryPrefNameList.indexOf(props.prefName) !== -1)
+      const aryPrefNameList = ary.map((props) => props.prefName)
+      const remainAry = dataList.filter((props) => aryPrefNameList.indexOf(props.prefName) !== -1)
       setDataList([...remainAry])
     }
   }, [checkList])
 
   return (
     <>
-      <PrefectureCheckBoxes prefecture={checkList} handler={toggleCheckFlgHandler}/>
+      <PrefectureCheckBoxes prefecture={checkList} handler={toggleCheckFlgHandler} />
       <Graph data={dataList} />
     </>
   )
