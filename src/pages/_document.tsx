1319c1ddbc1086d5186af3940e6da98e20042aba
sos05o <r.arihara.sys20@morijyobi.ac.jp>
2022-12-22 12:51:21 +0900

    [Feat] RESASからのデータ取得関数の作成
    グラフデータとして使用する為に、RESAS APIから特定のデータを取得する関数を作成しました。

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='ja'>
+      <Head title={"人口増加グラフ"}/>
       <body>
         <Main />
         <NextScript />
